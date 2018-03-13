a2018d13daadd25a22056d6794c4ef6cb4bd41f4
David Hessler <davidh.092705@gmail.com>
2018-03-13 15:15:10 -0400

    Added mounting of EFS volume

---
+++ ArtifactoryUserData.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum update -y
+#Mount Drives
+sudo yum install nfs-utils -y
+mkdir /data/artifactory
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-9c619ad4.efs.us-east-1.amazonaws.com:/ /data/artifactory
+
+#Pull Images
+curl https://raw.githubusercontent.com/JFrogDev/artifactory-docker-examples/master/docker-compose/artifactory-oss-postgresql.yml > docker-compose.yml
+docker-compose pull
+
+#Kick Off Compose
+docker-compose up
