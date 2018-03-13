f7c1bc994337c88907bfbc7dbc6d4c8d47d8bf15
David Hessler <davidh.092705@gmail.com>
2018-03-13 15:31:07 -0400

    Update ArtifactoryUserData.sh

--- ArtifactoryUserData.sh
+++ ArtifactoryUserData.sh
@@ -4,7 +4,8 @@ sudo yum update -y
 #Mount Drives
 sudo yum install nfs-utils -y
 mkdir /data/artifactory
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-9c619ad4.efs.us-east-1.amazonaws.com:/ /data/artifactory
+mkdir /data/postgresql
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-9c619ad4.efs.us-east-1.amazonaws.com:/ /data/postgresql
 
 #Pull Images
 curl https://raw.githubusercontent.com/JFrogDev/artifactory-docker-examples/master/docker-compose/artifactory-oss-postgresql.yml > docker-compose.yml
